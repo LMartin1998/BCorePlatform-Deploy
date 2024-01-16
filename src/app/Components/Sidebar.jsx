68ba036ab9f613c68e3aea812600c6b925f31543
lmartin1998 <jlmh_aguila@hotmail.com>
2024-01-16 10:27:49 -0600

    Firs card and show sidebar

--- src/app/Components/Sidebar.jsx
+++ src/app/Components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import {RxDashboard} from 'react-icons/rx';
+import { IoHomeOutline } from "react-icons/io5";
 import { CiSquareChevLeft } from "react-icons/ci";
 
 const Sidebar = ({ children, show, setter}) => {
@@ -9,7 +9,6 @@ const Sidebar = ({ children, show, setter}) => {
   return (
     <div className="flex">
       <div className={`${show ? "w-72" : "w-20"} fixed h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between`}>
-      {/* <div className="fixed w-20 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between"> */}
         <div className="flex gap-x-4 flex-col ">
           <Link href="/">
             <div className="bg-blue-800 p-1 rounded-lg inline-block">
@@ -19,13 +18,18 @@ const Sidebar = ({ children, show, setter}) => {
           <span className="border-b-[1px] border-gray-200 w-full p-2"></span>
           <Link href='/'>
             <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block flex'>
-              <RxDashboard size={25} />
-                <p className={`${show ? "" : "hidden"} pl-4`}>Teams</p>
+              <div className="flex justify-center items-center">
+                <IoHomeOutline className="flex justify-center " size={25} />
+              </div>
+              <div className={`${show ? "" : "hidden"} w-full text-center`}>
+                <h2 className=" text-gray-700">Teams</h2>
+                <p className="text-gray-400">Happy Arkansas</p>
+              </div>
             </div>
           </Link>
           <span className="border-b-[1px] border-gray-200 w-full p-2"></span>
-          <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block flex justify-center'>
-            <CiSquareChevLeft onClick={()=> {setter(oldval => !oldval )}} size={25} />
+          <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block flex justify-center' onClick={()=> {setter(oldval => !oldval )}}>
+            <CiSquareChevLeft size={25} />
           </div>
         </div>
       </div>
