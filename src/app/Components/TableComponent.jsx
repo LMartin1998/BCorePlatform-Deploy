aa326219e55c0af9a36954df44935df2967154ab
XknopiX117 <jerespartan117@hotmail.com>
2024-01-26 15:20:10 -0600

    working on table

--- src/app/Components/TableComponent.jsx
+++ src/app/Components/TableComponent.jsx
@@ -11,6 +11,7 @@ import data from "@/app/data/docdata.jsx";
 import { HiFolderDownload } from "react-icons/hi";
 import { RiEdit2Fill } from "react-icons/ri";
 import { AiOutlineSortAscending, AiOutlineSortDescending } from "react-icons/ai";
+import { MdDriveFolderUpload } from "react-icons/md";
 
 export default function TableComponent() {
 
@@ -44,52 +45,59 @@ export default function TableComponent() {
     });
 
     return (
-        <table className='w-full'>
-            <thead>
-                {table.getHeaderGroups().map((headerGroup) => (
-                    <tr className='text-left border-b border-solid border-black select-none' key={headerGroup.id}>
-                        {headerGroup.headers.map((header) => (
-                            <th className='text-left p-2 border-b border-solid border-black'
-                                key={header.id}
-                                onClick={header.column.getToggleSortingHandler()}
-                            >
-                                {header.isPlaceholder ? null : (
-                                    <div>
-                                        {flexRender(
-                                            header.column.columnDef.header,
-                                            header.getContext()
-                                        )}
-                                        {['asc', 'desc'].includes(
-                                            header.column.getIsSorted()
-                                        ) && (
-                                                <span>
-                                                    {header.column.getIsSorted() === 'asc' ? <AiOutlineSortAscending size={20} /> : <AiOutlineSortDescending size={20} />}
-                                                </span>
+        <>
+            <div className='w-full flex justify-end'>
+                <button className='m-5'>
+                    <MdDriveFolderUpload size={50} className='bg-white border border-solid border-black rounded hover:bg-blue-200' />
+                </button>
+            </div>
+            <table className='w-full'>
+                <thead>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <tr className='text-left border-b border-solid border-black select-none' key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => (
+                                <th className='text-left p-2 border-b border-solid border-black'
+                                    key={header.id}
+                                    onClick={header.column.getToggleSortingHandler()}
+                                >
+                                    {header.isPlaceholder ? null : (
+                                        <div>
+                                            {flexRender(
+                                                header.column.columnDef.header,
+                                                header.getContext()
                                             )}
-                                    </div>
-                                )}
-                            </th>
-                        ))}
-                    </tr>
-                ))}
-            </thead>
-            <tbody>
-                {table.getRowModel().rows.map((row) => (
-                    <tr key={row.id} className={'bg-white border-b border-solid border-black hover:bg-gray-200'}>
-                        {row.getVisibleCells().map((cell) => (
-                            <td key={cell.id} className='py-4' onClick={handleDoc}>
-                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                            {['asc', 'desc'].includes(
+                                                header.column.getIsSorted()
+                                            ) && (
+                                                    <span>
+                                                        {header.column.getIsSorted() === 'asc' ? <AiOutlineSortAscending size={20} /> : <AiOutlineSortDescending size={20} />}
+                                                    </span>
+                                                )}
+                                        </div>
+                                    )}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody>
+                    {table.getRowModel().rows.map((row) => (
+                        <tr key={row.id} className={'bg-white border-b border-solid border-black hover:bg-blue-100 hover:font-bold'}>
+                            {row.getVisibleCells().map((cell) => (
+                                <td key={cell.id} className='py-4 px-2' onClick={handleDoc}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </td>
+                            ))}
+                            <td>
+                                <HiFolderDownload size={25} className='hover:cursor-pointer' />
                             </td>
-                        ))}
-                        <td>
-                            <HiFolderDownload size={25} className='hover:cursor-pointer' />
-                        </td>
-                        <td>
-                            <RiEdit2Fill size={25} className='hover:cursor-pointer' />
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+                            <td>
+                                <RiEdit2Fill size={25} className='hover:cursor-pointer' />
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
     );
 };
