d4ec604e97e28456120172e8eb46516ad4f513f4
XknopiX117 <jerespartan117@hotmail.com>
2024-01-30 12:59:25 -0600

    wip docs table

--- src/app/Components/DocsTableComponent.jsx
+++ src/app/Components/DocsTableComponent.jsx
@@ -11,7 +11,10 @@ import data from "@/app/data/docdata.jsx";
 import { HiFolderDownload } from "react-icons/hi";
 import { RiEdit2Fill } from "react-icons/ri";
 import { AiOutlineSortAscending, AiOutlineSortDescending } from "react-icons/ai";
-import { MdDriveFolderUpload } from "react-icons/md";
+import { MdFirstPage, MdChevronLeft, MdChevronRight, MdLastPage } from 'react-icons/md';
+import { HiOutlineTrash } from "react-icons/hi2";
+import { IoFilterOutline } from "react-icons/io5";
+import { BiSortAlt2 } from "react-icons/bi";
 import { useState } from 'react';
 
 export default function DocsTable() {
@@ -73,17 +76,22 @@ export default function DocsTable() {
 
     return (
         <>
-            <div className='w-full flex justify-end'>
-                <button className='m-5'>
-                    <MdDriveFolderUpload size={50} className='bg-white border border-solid border-black rounded hover:bg-blue-200' />
+            <div className='w-full flex justify-between'>
+                <button className='bg-orange-500 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-orange-600'>
+                    + Add document
                 </button>
+                <div className='flex space-x-2'>
+                    <button className='flex items-center text-gray-700 hover:bg-slate-400 hover:rounded-lg hover:text-white py-1 px-3'><HiOutlineTrash size={22} className='mr-1' />Delete</button>
+                    <button className='flex items-center text-gray-700 hover:bg-slate-400 hover:rounded-lg hover:text-white py-1 px-3'><IoFilterOutline size={22} className='mr-1' />Filter</button>
+                    <button className='flex items-center text-gray-700 hover:bg-slate-400 hover:rounded-lg hover:text-white py-1 px-3'><BiSortAlt2 size={22} className='mr-1' />Sort</button>
+                </div>
             </div>
-            <table className='w-full'>
+            <table className='w-full mt-3'>
                 <thead>
                     {table.getHeaderGroups().map((headerGroup) => (
-                        <tr className='text-left border-b border-solid border-black select-none' key={headerGroup.id}>
+                        <tr className='text-left bg-gray-200 select-none' key={headerGroup.id}>
                             {headerGroup.headers.map((header) => (
-                                <th className='text-left p-2 border-b border-solid border-black'
+                                <th className='text-left p-1 text-gray-700 border-b border-solid border-gray-400'
                                     key={header.id}
                                     onClick={header.column.getToggleSortingHandler()}
                                 >
@@ -109,9 +117,9 @@ export default function DocsTable() {
                 </thead>
                 <tbody>
                     {table.getRowModel().rows.map((row) => (
-                        <tr key={row.id} className={'bg-white border-b border-solid border-black hover:bg-blue-100 hover:font-bold'}>
+                        <tr key={row.id} className={'bg-white border-b border-solid border-gray-400 hover:bg-gray-100'}>
                             {row.getVisibleCells().map((cell) => (
-                                <td key={cell.id} className='py-4 px-2' onClick={handleDoc}>
+                                <td key={cell.id} className='py-2 px-1' onClick={handleDoc}>
                                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                                 </td>
                             ))}
@@ -119,12 +127,12 @@ export default function DocsTable() {
                     ))}
                 </tbody>
             </table>
-            <button onClick={() => table.setPageIndex(0)}>Primer Pagina</button>
-            <button onClick={() => table.previousPage()}>Pagina Anterior</button>
-            <button onClick={() => table.nextPage()}>Pagina Siguiente</button>
-            <button onClick={() => table.setPageIndex(table.getPageCount() - 1)}>
-                Ultima Pagina
-            </button>
+            <div className='flex justify-end space-x-2 mt-2'>
+                <button className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600' onClick={() => table.setPageIndex(0)}><MdFirstPage /></button>
+                <button className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600' onClick={() => table.previousPage()}><MdChevronLeft /></button>
+                <button className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600' onClick={() => table.nextPage()}><MdChevronRight /></button>
+                <button className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600' onClick={() => table.setPageIndex(table.getPageCount() - 1)}><MdLastPage /></button>
+            </div>
         </>
     );
 };
