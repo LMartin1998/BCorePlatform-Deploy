b2ecddfed381a6426356d82f4693440dd7e8230b
XknopiX117 <jerespartan117@hotmail.com>
2024-01-31 11:17:33 -0600

    docs done, needs style

--- src/app/Components/DocsTableComponent.jsx
+++ src/app/Components/DocsTableComponent.jsx
@@ -1,48 +1,92 @@
 "use client";
-import { flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, useReactTable } from "@tanstack/react-table";
+import {
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    useReactTable,
+} from "@tanstack/react-table";
 import docs from "../data/docdata";
 import { useState } from "react";
-import { MdFirstPage, MdChevronLeft, MdChevronRight, MdLastPage } from 'react-icons/md';
+import {
+    MdFirstPage,
+    MdChevronLeft,
+    MdChevronRight,
+    MdLastPage,
+} from "react-icons/md";
 import { FaFileWord, FaFilePdf } from "react-icons/fa";
 import { GiMechanicalArm, GiElectricalResistance } from "react-icons/gi";
 
-
 export default function DocsTable() {
-
     const extensionLabel = {
-        "docx": <FaFileWord size={25} />,
-        "pdf": <FaFilePdf size={25} />,
+        docx: <FaFileWord size={25} />,
+        pdf: <FaFilePdf size={25} />,
     };
 
     const typeLabel = {
-        "electrical": <GiElectricalResistance size={25} />,
-        "mechanical": <GiMechanicalArm size={25} />,
+        Electrical: <GiElectricalResistance size={25} />,
+        Mechanical: <GiMechanicalArm size={25} />,
     };
 
     const [rowSelection, setRowSelection] = useState({});
 
     const columns = [
         {
-            id: 'selector-column',
-            header: ({ table }) => <input type="checkbox" checked={table.getIsAllRowsSelected() || table.getIsSomeRowsSelected()}
-                onChange={table.getToggleAllRowsSelectedHandler()}></input>,
-            cell: ({ row }) => <input type="checkbox" checked={row.getIsSelected()}
-                disabled={!row.getCanSelect()} onChange={row.getToggleSelectedHandler()}></input>
+            id: "selector-column",
+            header: ({ table }) => (
+                <input
+                    type="checkbox"
+                    checked={
+                        table.getIsAllRowsSelected() ||
+                        table.getIsSomeRowsSelected()
+                    }
+                    onChange={table.getToggleAllRowsSelectedHandler()}
+                ></input>
+            ),
+            cell: ({ row }) => (
+                <input
+                    type="checkbox"
+                    checked={row.getIsSelected()}
+                    disabled={!row.getCanSelect()}
+                    onChange={row.getToggleSelectedHandler()}
+                ></input>
+            ),
         },
         {
-            header: 'Name',
-            accessorKey: 'name',
-            cell: row => <p>{row.getValue()}</p>
+            header: "Name",
+            accessorKey: "name",
+            cell: (row) => <p>{row.getValue()}</p>,
         },
         {
-            header: 'Type',
-            accessorKey: 'type',
-            cell: row => <p>{typeLabel[row.getValue()]}{row.getValue()}</p>
+            header: "Owner",
+            accessorKey: "owner",
+            cell: (row) => <p>{row.getValue()}</p>,
         },
         {
-            header: 'Extension',
-            accessorKey: 'extension',
-            cell: row => <p>{extensionLabel[row.getValue()]}{row.getValue()}</p>
+            header: "Tags",
+            accessorKey: "tags",
+            cell: (row) => <div>
+                {row.getValue().map((tag, index) => (
+                    <div key={index} className="bg-gray-100 justify-center">
+                        <p>{tag}</p>
+                    </div>
+                ))}
+            </div>
+        },
+        {
+            header: "Extension",
+            accessorKey: "extension",
+            cell: (row) => (
+                <p>
+                    {extensionLabel[row.getValue()]}
+                    {row.getValue()}
+                </p>
+            ),
+        },
+        {
+            header: 'Size',
+            accessorKey: "size",
+            cell: (row) => <p>{row.getValue()}</p>
         },
     ];
 
@@ -63,60 +107,88 @@ export default function DocsTable() {
 
     return (
         <>
-            <table className='w-full mt-3'>
+            <table className="w-full mt-3">
                 <thead>
-                    {
-                        table.getHeaderGroups().map(headerGroup => (
-                            <tr className='text-left bg-gray-200 select-none' key={headerGroup.id}>
-                                {
-                                    headerGroup.headers.map(header => (
-                                        <th className='text-left p-1 text-gray-700 border-b border-solid border-gray-400' key={header.id}>
-                                            {
-                                                header.isPlaceholder ? null
-                                                    : flexRender(
-                                                        header.column.columnDef.header,
-                                                        header.getContext()
-                                                    )
-                                            }
-                                        </th>
-                                    ))
-                                }
-                            </tr>
-                        ))
-                    }
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <tr
+                            className="text-left bg-gray-200 select-none"
+                            key={headerGroup.id}
+                        >
+                            {headerGroup.headers.map((header) => (
+                                <th
+                                    className="text-left p-1 text-gray-700 border-b border-solid border-gray-400"
+                                    key={header.id}
+                                >
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
                 </thead>
                 <tbody>
-                    {table.getRowModel().rows.map(row => {
+                    {table.getRowModel().rows.map((row) => {
                         return (
-                            <tr className='bg-white border-b border-solid border-gray-400 hover:bg-gray-100' key={row.id}>
-                                {row.getVisibleCells().map(cell => {
+                            <tr
+                                className="bg-white border-b border-solid border-gray-400 hover:bg-gray-100"
+                                key={row.id}
+                            >
+                                {row.getVisibleCells().map((cell) => {
                                     return (
-                                        <td className='py-2 px-1' key={cell.id}>
+                                        <td className="py-2 px-1" key={cell.id}>
                                             {flexRender(
                                                 cell.column.columnDef.cell,
                                                 cell.getContext()
                                             )}
                                         </td>
-                                    )
+                                    );
                                 })}
                             </tr>
-                        )
+                        );
                     })}
                 </tbody>
             </table>
-            <div className='flex justify-end space-x-2 mt-2'>
-                <button className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600' onClick={() => table.setPageIndex(0)} disabled={!table.getCanPreviousPage()}><MdFirstPage /></button>
-                <button className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600' onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}><MdChevronLeft /></button>
-                <button className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600' onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}><MdChevronRight /></button>
-                <button className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600' onClick={() => table.setPageIndex(table.getPageCount() - 1)} disabled={!table.getCanNextPage()}><MdLastPage /></button>
+            <div className="flex justify-end space-x-2 mt-2">
+                <button
+                    className="bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600"
+                    onClick={() => table.setPageIndex(0)}
+                    disabled={!table.getCanPreviousPage()}
+                >
+                    <MdFirstPage />
+                </button>
+                <button
+                    className="bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600"
+                    onClick={() => table.previousPage()}
+                    disabled={!table.getCanPreviousPage()}
+                >
+                    <MdChevronLeft />
+                </button>
+                <button
+                    className="bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600"
+                    onClick={() => table.nextPage()}
+                    disabled={!table.getCanNextPage()}
+                >
+                    <MdChevronRight />
+                </button>
+                <button
+                    className="bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600"
+                    onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+                    disabled={!table.getCanNextPage()}
+                >
+                    <MdLastPage />
+                </button>
                 <select
-                    className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600'
+                    className="bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600"
                     value={table.getState().pagination.pageSize}
-                    onChange={e => {
-                        table.setPageSize(Number(e.target.value))
+                    onChange={(e) => {
+                        table.setPageSize(Number(e.target.value));
                     }}
                 >
-                    {[1, 2, 3, 4, 5].map(pageSize => (
+                    {[1, 2, 3, 4, 5].map((pageSize) => (
                         <option key={pageSize} value={pageSize}>
                             Show {pageSize}
                         </option>
@@ -125,4 +197,4 @@ export default function DocsTable() {
             </div>
         </>
     );
-};
+}
--- src/app/data/docdata.jsx
+++ src/app/data/docdata.jsx
@@ -1,28 +1,36 @@
 const docs = [
-    {
-        id: 0,
-        name: "doc0.pdf",
-        type: 'electrical',
-        extension: "pdf",
-    },
-    {
-        id: 1,
-        name: "doc1.pdf",
-        type: 'mechanical',
-        extension: "pdf",
-    },
-    {
-        id: 2,
-        name: "doc2.docx",
-        type: 'electrical',
-        extension: "docx",
-    },
-    {
-        id: 3,
-        name: "doc3.docx",
-        type: 'electrical',
-        extension: "docx",
-    },
+  {
+    id: 0,
+    name: "doc0.pdf",
+    tags: ["Electrical", "Mechanical"],
+    extension: "pdf",
+    owner: "Jeronimo Molina",
+    size: "1.1MB",
+  },
+  {
+    id: 1,
+    name: "map1.pdf",
+    tags: ["Electrical", "Mechanical"],
+    extension: "pdf",
+    owner: "Alex Arevalo",
+    size: "1.1MB",
+  },
+  {
+    id: 2,
+    name: "map1info.docx",
+    tags: ["Electrical"],
+    extension: "docx",
+    owner: "Alex Arevalo",
+    size: "700KB",
+  },
+  {
+    id: 3,
+    name: "doc1.pdf",
+    tags: ["Mechanical"],
+    extension: "pdf",
+    owner: "Luizandro Martin",
+    size: "1.1MB",
+  },
 ];
 
 export default docs;
