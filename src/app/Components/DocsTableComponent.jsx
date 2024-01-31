89f5d038cb673bab35f50f5e9826d258f8fa53c3
XknopiX117 <jerespartan117@hotmail.com>
2024-01-30 22:11:55 -0600

    early development for docs

--- src/app/Components/DocsTableComponent.jsx
+++ src/app/Components/DocsTableComponent.jsx
@@ -1,137 +1,127 @@
 "use client";
-import {
-    useReactTable,
-    getCoreRowModel,
-    flexRender,
-    getPaginationRowModel,
-    getSortedRowModel,
-    getFilteredRowModel,
-} from '@tanstack/react-table';
-import data from "@/app/data/docdata.jsx";
-import { HiFolderDownload } from "react-icons/hi";
-import { RiEdit2Fill } from "react-icons/ri";
-import { AiOutlineSortAscending, AiOutlineSortDescending } from "react-icons/ai";
+import { flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, useReactTable } from "@tanstack/react-table";
+import docs from "../data/docdata";
+import { useState } from "react";
 import { MdFirstPage, MdChevronLeft, MdChevronRight, MdLastPage } from 'react-icons/md';
-import { HiOutlineTrash } from "react-icons/hi2";
-import { IoFilterOutline } from "react-icons/io5";
-import { BiSortAlt2 } from "react-icons/bi";
-import { useState } from 'react';
+import { FaFileWord, FaFilePdf } from "react-icons/fa";
+import { GiMechanicalArm, GiElectricalResistance } from "react-icons/gi";
+
 
 export default function DocsTable() {
 
-    const [items, setItems] = useState(2);
-    const handleDoc = (e) => {
-        e.stopPropagation();
-        console.log(e.target);
-    }
+    const extensionLabel = {
+        "docx": <FaFileWord size={25} />,
+        "pdf": <FaFilePdf size={25} />,
+    };
+
+    const typeLabel = {
+        "electrical": <GiElectricalResistance size={25} />,
+        "mechanical": <GiMechanicalArm size={25} />,
+    };
+
+    const [rowSelection, setRowSelection] = useState({});
 
     const columns = [
         {
-            id: 'select',
-            header: ({ table }) => (
-                <input type='checkbox' checked={table.getIsAllRowsSelected()} onChange={table.getToggleAllRowsSelectedHandler()}></input>
-            ),
-            cell: ({ row }) => (
-                <input type='checkbox' checked={row.getIsSelected()} disabled={!row.getCanSelect()} onChange={row.getToggleSelectedHandler()}></input>
-            ),
-        },
-        {
-            header: 'ID',
-            accessorKey: 'id',
+            id: 'selector-column',
+            header: ({ table }) => <input type="checkbox" checked={table.getIsAllRowsSelected() || table.getIsSomeRowsSelected()}
+                onChange={table.getToggleAllRowsSelectedHandler()}></input>,
+            cell: ({ row }) => <input type="checkbox" checked={row.getIsSelected()}
+                disabled={!row.getCanSelect()} onChange={row.getToggleSelectedHandler()}></input>
         },
         {
             header: 'Name',
             accessorKey: 'name',
+            cell: row => <p>{row.getValue()}</p>
         },
         {
-            header: 'Size',
-            accessorKey: 'size',
+            header: 'Type',
+            accessorKey: 'type',
+            cell: row => <p>{typeLabel[row.getValue()]}{row.getValue()}</p>
         },
         {
-            id: 'download-action',
-            header: '',
-            cell: ({ row }) => <div><HiFolderDownload size={25} className='hover:cursor-pointer' /></div>
-        },
-        {
-            id: 'open-action',
-            header: '',
-            cell: ({ row }) => <div><RiEdit2Fill size={25} className='hover:cursor-pointer' /></div>
+            header: 'Extension',
+            accessorKey: 'extension',
+            cell: row => <p>{extensionLabel[row.getValue()]}{row.getValue()}</p>
         },
     ];
+
+    const [data, setData] = useState(docs);
 
     const table = useReactTable({
         data,
         columns,
+        state: {
+            rowSelection,
+        },
+        enableRowSelection: true,
+        onRowSelectionChange: setRowSelection,
         getCoreRowModel: getCoreRowModel(),
         getPaginationRowModel: getPaginationRowModel(),
-        getSortedRowModel: getSortedRowModel(),
         getFilteredRowModel: getFilteredRowModel(),
-        state: {
-            pagination: {
-                pageIndex: 0,
-                pageSize: items,
-            }
-        }
     });
 
     return (
         <>
-            <div className='w-full flex justify-between'>
-                <button className='bg-orange-500 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-orange-600'>
-                    + Add document
-                </button>
-                <div className='flex space-x-2'>
-                    <button className='flex items-center text-gray-700 hover:bg-slate-400 hover:rounded-lg hover:text-white py-1 px-3'><HiOutlineTrash size={22} className='mr-1' />Delete</button>
-                    <button className='flex items-center text-gray-700 hover:bg-slate-400 hover:rounded-lg hover:text-white py-1 px-3'><IoFilterOutline size={22} className='mr-1' />Filter</button>
-                    <button className='flex items-center text-gray-700 hover:bg-slate-400 hover:rounded-lg hover:text-white py-1 px-3'><BiSortAlt2 size={22} className='mr-1' />Sort</button>
-                </div>
-            </div>
             <table className='w-full mt-3'>
                 <thead>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                        <tr className='text-left bg-gray-200 select-none' key={headerGroup.id}>
-                            {headerGroup.headers.map((header) => (
-                                <th className='text-left p-1 text-gray-700 border-b border-solid border-gray-400'
-                                    key={header.id}
-                                    onClick={header.column.getToggleSortingHandler()}
-                                >
-                                    {header.isPlaceholder ? null : (
-                                        <div>
-                                            {flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext()
-                                            )}
-                                            {['asc', 'desc'].includes(
-                                                header.column.getIsSorted()
-                                            ) && (
-                                                    <span>
-                                                        {header.column.getIsSorted() === 'asc' ? <AiOutlineSortAscending size={20} /> : <AiOutlineSortDescending size={20} />}
-                                                    </span>
-                                                )}
-                                        </div>
-                                    )}
-                                </th>
-                            ))}
-                        </tr>
-                    ))}
+                    {
+                        table.getHeaderGroups().map(headerGroup => (
+                            <tr className='text-left bg-gray-200 select-none' key={headerGroup.id}>
+                                {
+                                    headerGroup.headers.map(header => (
+                                        <th className='text-left p-1 text-gray-700 border-b border-solid border-gray-400' key={header.id}>
+                                            {
+                                                header.isPlaceholder ? null
+                                                    : flexRender(
+                                                        header.column.columnDef.header,
+                                                        header.getContext()
+                                                    )
+                                            }
+                                        </th>
+                                    ))
+                                }
+                            </tr>
+                        ))
+                    }
                 </thead>
                 <tbody>
-                    {table.getRowModel().rows.map((row) => (
-                        <tr key={row.id} className={'bg-white border-b border-solid border-gray-400 hover:bg-gray-100'}>
-                            {row.getVisibleCells().map((cell) => (
-                                <td key={cell.id} className='py-2 px-1' onClick={handleDoc}>
-                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                </td>
-                            ))}
-                        </tr>
-                    ))}
+                    {table.getRowModel().rows.map(row => {
+                        return (
+                            <tr className='bg-white border-b border-solid border-gray-400 hover:bg-gray-100' key={row.id}>
+                                {row.getVisibleCells().map(cell => {
+                                    return (
+                                        <td className='py-2 px-1' key={cell.id}>
+                                            {flexRender(
+                                                cell.column.columnDef.cell,
+                                                cell.getContext()
+                                            )}
+                                        </td>
+                                    )
+                                })}
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
             <div className='flex justify-end space-x-2 mt-2'>
-                <button className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600' onClick={() => table.setPageIndex(0)}><MdFirstPage /></button>
-                <button className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600' onClick={() => table.previousPage()}><MdChevronLeft /></button>
-                <button className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600' onClick={() => table.nextPage()}><MdChevronRight /></button>
-                <button className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600' onClick={() => table.setPageIndex(table.getPageCount() - 1)}><MdLastPage /></button>
+                <button className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600' onClick={() => table.setPageIndex(0)} disabled={!table.getCanPreviousPage()}><MdFirstPage /></button>
+                <button className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600' onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}><MdChevronLeft /></button>
+                <button className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600' onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}><MdChevronRight /></button>
+                <button className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600' onClick={() => table.setPageIndex(table.getPageCount() - 1)} disabled={!table.getCanNextPage()}><MdLastPage /></button>
+                <select
+                    className='bg-gray-700 text-white px-3 py-1 rounded-md focus:outline-none hover:bg-slate-600'
+                    value={table.getState().pagination.pageSize}
+                    onChange={e => {
+                        table.setPageSize(Number(e.target.value))
+                    }}
+                >
+                    {[1, 2, 3, 4, 5].map(pageSize => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
             </div>
         </>
     );
--- src/app/data/docdata.jsx
+++ src/app/data/docdata.jsx
@@ -1,59 +1,28 @@
-const data = [
+const docs = [
     {
         id: 0,
         name: "doc0.pdf",
-        size: "10kb",
+        type: 'electrical',
+        extension: "pdf",
     },
     {
         id: 1,
         name: "doc1.pdf",
-        size: "15kb",
+        type: 'mechanical',
+        extension: "pdf",
     },
     {
         id: 2,
-        name: "doc2.pdf",
-        size: "15kb",
+        name: "doc2.docx",
+        type: 'electrical',
+        extension: "docx",
     },
     {
         id: 3,
-        name: "doc3.pdf",
-        size: "15kb",
-    },
-    {
-        id: 4,
-        name: "doc4.pdf",
-        size: "15kb",
-    },
-    {
-        id: 5,
-        name: "doc5.pdf",
-        size: "15kb",
-    },
-    {
-        id: 6,
-        name: "doc6.pdf",
-        size: "15kb",
-    },
-    {
-        id: 7,
-        name: "doc7.pdf",
-        size: "15kb",
-    },
-    {
-        id: 8,
-        name: "doc8.pdf",
-        size: "15kb",
-    },
-    {
-        id: 9,
-        name: "doc9.pdf",
-        size: "15kb",
-    },
-    {
-        id: 10,
-        name: "doc10.pdf",
-        size: "15kb",
+        name: "doc3.docx",
+        type: 'electrical',
+        extension: "docx",
     },
 ];
 
-export default data;
+export default docs;
