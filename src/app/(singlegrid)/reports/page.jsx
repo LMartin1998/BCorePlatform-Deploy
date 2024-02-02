e5c7fe8e23e61cc14b4e2bbe8f44723c29e1abf4
lmartin1998 <jlmh_aguila@hotmail.com>
2024-02-02 15:49:11 -0600

    Reports Stepper

--- src/app/(singlegrid)/reports/page.jsx
+++ src/app/(singlegrid)/reports/page.jsx
@@ -1,13 +1,47 @@
+"use client";
+import Stepper from "@/app/components/Stepper";
 import Header from "../../components/Header";
 import TopCards from "../../components/TopCards";
+import StepperControl from "@/app/components/StepperControl";
+import SelectBlock from "@/app/components/steps/SelectBlock";
+import TaskInfo from "@/app/components/steps/TaskInfo";
+import { useState } from "react";
 
 export default function Reports() {
+    const [currentStep, setCurrentStep] = useState(1);
+    
+    const steps = [
+        "Block",
+        "Task",
+        "Resume"
+    ];
+
+    const displayStep = (step) => {
+        switch(step) {
+            case 1:
+                return <SelectBlock></SelectBlock>
+            case 2: 
+                return <TaskInfo></TaskInfo>
+            case 3:
+                return <Resume></Resume>
+            default:
+        }
+    }
+    
     return (
         <main className="bg-gray-100 min-h-screen">
             <Header></Header>
             <TopCards></TopCards>
             <div className="p-4">
-                <p>Reports page</p>
+                <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
+                    <div className="container horizontal mt-5">
+                        <Stepper
+                            steps = {steps}
+                            currentStep = {currentStep}
+                        ></Stepper>
+                    </div>
+                    <StepperControl></StepperControl>
+                </div>
             </div>
         </main>
     );
---
+++ src/app/Components/steps/SelectBlock.jsx
@@ -0,0 +1,4 @@
+
+export default function SelectBlock(){
+    return <>Choose a block</>
+}
