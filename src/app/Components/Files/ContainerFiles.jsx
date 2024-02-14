b0afcbf58087e5397c08fedcb82a3225c4dad880
XknopiX117 <jerespartan117@hotmail.com>
2024-02-14 14:45:00 -0600

    working on error

--- src/app/Components/Files/ContainerFiles.jsx
+++ src/app/Components/Files/ContainerFiles.jsx
@@ -3,30 +3,28 @@ import { IoFolderOpenOutline } from "react-icons/io5";
 export default function ContainerFiles({ filesList, updateChildrenFiles }) {
   const renderChildrenFiles = () => {
     return filesList.map((file) => (
-      <div className="flex">
+      <div
+        className="flex items-center select-none"
+        key={file.id}
+        id={file.id}
+        onDoubleClick={updateChildrenFiles}
+      >
         <input type="checkbox"></input>
-        <div
-          className="flex items-center select-none"
-          key={file.id}
-          id={file.id}
-          onDoubleClick={updateChildrenFiles}
-        >
-          {file.isFolder ? (
-            <>
-              <IoFolderOpenOutline
-                size={40}
-                className="m-1"
-              ></IoFolderOpenOutline>
-            </>
-          ) : (
-            <img
-              className="size-10 m-1 "
-              src={file.imageLink}
-              alt={file.fileName}
-            />
-          )}
-          <p>{file.fileName}</p>
-        </div>
+        {file.isFolder ? (
+          <>
+            <IoFolderOpenOutline
+              size={40}
+              className="m-1"
+            ></IoFolderOpenOutline>
+          </>
+        ) : (
+          <img
+            className="size-10 m-1 "
+            src={file.imageLink}
+            alt={file.fileName}
+          />
+        )}
+        <p>{file.fileName}</p>
       </div>
     ));
   };
