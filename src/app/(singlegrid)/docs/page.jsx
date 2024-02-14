1a75aa13a7b9bd3bb811b2623b914ddbee8e30a8
XknopiX117 <jerespartan117@hotmail.com>
2024-02-14 11:42:37 -0600

    Adding dynamic routing

--- src/app/(singlegrid)/docs/page.jsx
+++ src/app/(singlegrid)/docs/page.jsx
@@ -5,8 +5,11 @@ import Header from "../../components/Header";
 import TopCards from "../../components/TopCards";
 import SidebarFiles from "@/app/components/Files/SidebarFiles";
 import ContainerFiles from "@/app/components/Files/ContainerFiles";
+import Link from "next/link";
 
 export default function Docs() {
+  const [path, setPath] = useState("/");
+
   const [rootFiles, setRootFiles] = useState(
     filesList.filter((file) => file.parentId === null)
   );
@@ -23,12 +26,14 @@ export default function Docs() {
     e.stopPropagation();
     setRootId(Number(e.currentTarget.id));
     setChildrenFiles(rootFiles[Number(e.currentTarget.id)].children);
+    setPath(`/docs/${Number(e.currentTarget.id)}`);
   };
 
   const updateChildrenFiles = (e) => {
     e.stopPropagation();
     setChildrenId(Number(e.currentTarget.id));
     setChildrenFiles(childrenFiles[Number(e.currentTarget.id)].children);
+    setPath(`${path}/${Number(e.currentTarget.id)}`);
   };
 
   return (
@@ -38,6 +43,7 @@ export default function Docs() {
       <div className="flex m-1">
         {rootFiles && (
           <div className="m-1 w-1/5 h-full grid">
+            <p>Name</p>
             <SidebarFiles
               filesList={rootFiles}
               updateRootFiles={updateRootFiles}
@@ -45,7 +51,11 @@ export default function Docs() {
           </div>
         )}
         {childrenFiles && (
-          <div className="w-full h-full grid">
+          <div className="m-1 w-full h-full grid">
+            <p>
+              <Link href={path}>{path}</Link>
+            </p>
+            <p>Files</p>
             <ContainerFiles
               filesList={childrenFiles}
               updateChildrenFiles={updateChildrenFiles}
