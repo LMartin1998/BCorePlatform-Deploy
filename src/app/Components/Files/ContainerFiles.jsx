6e1e9899cb81107cda14ee7baf0eb686b5a1e816
XknopiX117 <jerespartan117@hotmail.com>
2024-02-14 14:23:13 -0600

    checkboxes fixed

--- src/app/Components/Files/ContainerFiles.jsx
+++ src/app/Components/Files/ContainerFiles.jsx
@@ -3,28 +3,30 @@ import { IoFolderOpenOutline } from "react-icons/io5";
 export default function ContainerFiles({ filesList, updateChildrenFiles }) {
   const renderChildrenFiles = () => {
     return filesList.map((file) => (
-      <div
-        className="flex items-center select-none"
-        key={file.id}
-        id={file.id}
-        onDoubleClick={updateChildrenFiles}
-      >
+      <div className="flex">
         <input type="checkbox"></input>
-        {file.isFolder ? (
-          <>
-            <IoFolderOpenOutline
-              size={40}
-              className="m-1"
-            ></IoFolderOpenOutline>
-          </>
-        ) : (
-          <img
-            className="size-10 m-1 "
-            src={file.imageLink}
-            alt={file.fileName}
-          />
-        )}
-        <p>{file.fileName}</p>
+        <div
+          className="flex items-center select-none"
+          key={file.id}
+          id={file.id}
+          onDoubleClick={updateChildrenFiles}
+        >
+          {file.isFolder ? (
+            <>
+              <IoFolderOpenOutline
+                size={40}
+                className="m-1"
+              ></IoFolderOpenOutline>
+            </>
+          ) : (
+            <img
+              className="size-10 m-1 "
+              src={file.imageLink}
+              alt={file.fileName}
+            />
+          )}
+          <p>{file.fileName}</p>
+        </div>
       </div>
     ));
   };
