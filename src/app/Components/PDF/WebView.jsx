7a5ed52b97028f6fc01d1ed5d7823f1683d94677
lmartin1998 <jlmh_aguila@hotmail.com>
2024-02-15 09:51:29 -0600

    Add table for webview

--- src/app/Components/PDF/WebView.jsx
+++ src/app/Components/PDF/WebView.jsx
@@ -1,24 +1,3 @@
-import { StyleSheet } from "@react-pdf/renderer";
-
-const styles = StyleSheet.create({
-    table: { 
-        display: "table", 
-        width: "auto", 
-        borderStyle: "solid", 
-        borderWidth: 1, 
-        borderRightWidth: 0, 
-        borderBottomWidth: 0 
-      }, 
-      tableRow: { 
-        margin: "auto", 
-        flexDirection: "row" 
-      }, 
-      tableCell: { 
-        margin: "auto", 
-        marginTop: 5, 
-        fontSize: 10 
-      }
-});
 
 const WebView = () => {
     return (
@@ -81,13 +60,51 @@ const WebView = () => {
             {/* Report */}
             <div style={{marginTop:"12px"}}>
                 <div style={{backgroundColor: "#3e5bb7", color:"white"}}>
-                    <p>Block PCS401</p>
+                    <p style={{textAlign:"center"}}>Block PCS401</p>
                 </div>
             </div>
-
+            <table style={{ borderCollapse: 'collapse', width: '100%' }}>
+                <thead>
+                <tr>
+                    <th style={styles.header}>Header 1</th>
+                    <th style={styles.header}>Header 2</th>
+                    <th style={styles.header}>Header 3</th>
+                    <th style={styles.header}>Header 4</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td style={styles.cell}>Dato 1</td>
+                    <td style={styles.cell}>Dato 2</td>
+                    <td style={styles.cell}>Dato 3</td>
+                    <td style={styles.cell}>Dato 4</td>
+                </tr>
+                <tr>
+                    <td style={styles.cell}>Dato 5</td>
+                    <td style={styles.cell}>Dato 6</td>
+                    <td style={styles.cell}>Dato 7</td>
+                    <td style={styles.cell}>Dato 8</td>
+                </tr>
+                {/* Puedes añadir más filas según sea necesario */}
+                </tbody>
+            </table>
         </div>
-
     );
 }
+
+const styles = {
+    header: {
+      backgroundColor: '#6179C8',
+      border: '1px solid black',
+      padding: '8px',
+      textAlign: 'center',
+      color: "white"
+    },
+    cell: {
+      border: '1px solid black',
+      padding: '8px',
+      textAlign: 'center',
+    },
+  };
  
 export default WebView;
