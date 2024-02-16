e215052204aed638ef998543c8b4dec75ce87402
XknopiX117 <jerespartan117@hotmail.com>
2024-02-16 16:48:52 -0600

    working on events

--- src/app/Components/files/ChildrenFiles.jsx
+++ src/app/Components/files/ChildrenFiles.jsx
@@ -1,28 +1,53 @@
 import { IoFolderOpenOutline } from "react-icons/io5";
 import DropdrownFiles from "./DropdownFiles";
+import { useEffect, useRef, useState } from "react";
 
 export default function ChildrenFiles({ filesList, updateChildrenFiles }) {
+  const [open, setOpen] = useState(-1);
+  const updateOpen = (e) => {
+    e.stopPropagation();
+    setOpen(Number(e.currentTarget.id));
+  };
+
+  const divRef = useRef(null);
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (divRef.current && !divRef.current.contains(e.target)) {
+        setOpen(-1);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   const renderChildrenFiles = () => {
     return filesList.map((file) => (
       <div
-        className="flex items-center select-none border rounded-lg border-gray-400 bg-white hover:bg-gray-100"
+        className={`h-12 w-full flex items-center select-none b0rder border-t border-b border-gray-400
+        ${
+          open === file.id
+            ? "bg-blue-200 hover:none"
+            : "bg-white hover:bg-gray-100"
+        }
+        `}
         key={file.id}
         id={file.id}
+        onClick={updateOpen}
         onDoubleClick={updateChildrenFiles}
       >
-        <div className="flex ml-2 w-1/6">
-          <input type="checkbox"></input>
-        </div>
         {file.isFolder ? (
-          <div className="flex justify-center w-1/6">
-            <IoFolderOpenOutline className="size-10"></IoFolderOpenOutline>
+          <div className="flex justify-center items-center m-1 w-1/6 h-6">
+            <IoFolderOpenOutline className="w-full h-full"></IoFolderOpenOutline>
           </div>
         ) : (
-          <div className="flex justify-center w-1/6">
-            <img className="size-10" src={file.imageLink} alt={file.fileName} />
+          <div className="flex justify-center items-center m-1 w-1/6 h-6">
+            <img className="size-6" src={file.imageLink} alt={file.fileName} />
           </div>
         )}
-        <div className="flex justify-center w-5/6">
+        <div className="flex justify-center w-2/3">
           <p>{file.fileName}</p>
         </div>
         <DropdrownFiles id={file.id}></DropdrownFiles>
@@ -31,11 +56,7 @@ export default function ChildrenFiles({ filesList, updateChildrenFiles }) {
   };
 
   return (
-    <div className="w-full h-full m-1 p-1 grid items-center border border-black rounded-lg bg-white">
-      <div className="flex w-1/6 justify-evenly hover:cursor-pointer">
-        <input type="checkbox"></input>
-        <p>Files</p>
-      </div>
+    <div className="w-full h-full m-1 p-1 grid items-center" ref={divRef}>
       {filesList && renderChildrenFiles()}
     </div>
   );
