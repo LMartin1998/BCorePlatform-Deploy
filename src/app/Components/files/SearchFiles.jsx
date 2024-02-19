0eca5de19c30b54961f39bb270348af85f86ee42
XknopiX117 <jerespartan117@hotmail.com>
2024-02-19 16:43:53 -0600

    modified:   src/app/Components/files/SearchFiles.jsx

--- src/app/Components/files/SearchFiles.jsx
+++ src/app/Components/files/SearchFiles.jsx
@@ -2,7 +2,7 @@ export default function SearchFiles({ searchPage }) {
   return (
     <div className="flex w-1/5 m-1">
       <input
-        className="border border-gray-500 rounded focus:outline-none focus:ring-black focus:ring-1 p-1"
+        className="border border-gray-500 rounded-lg focus:outline-none focus:ring-black focus:ring-1 p-1"
         type="text"
         placeholder="Search..."
         onChange={searchPage}
