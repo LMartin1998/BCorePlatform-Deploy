341c04924fa708cb5f44f1b802e90ad27a274927
XknopiX117 <jerespartan117@hotmail.com>
2024-02-20 12:02:13 -0600

    search done

--- src/app/Components/files/SearchFiles.jsx
+++ src/app/Components/files/SearchFiles.jsx
@@ -2,7 +2,7 @@ export default function SearchFiles({ searchPage }) {
   return (
     <div className="flex w-1/2 p-1 m-1">
       <input
-        className="p-2 w-full border border-gray-500 rounded-lg focus:outline-none focus:ring-black focus:ring-1 p-1"
+        className="p-2 w-full border border-gray-500 rounded-lg focus:outline-none focus:ring-black focus:ring-1"
         type="text"
         placeholder="Search..."
         onChange={searchPage}
