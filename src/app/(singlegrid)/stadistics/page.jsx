07397f0558db5fb390c905f9856ec2038c48b500
Jerónimo Molina <jerespartan117@hotmail.com>
2024-03-04 16:23:25 -0600

    adding modal of block list

--- src/app/(singlegrid)/stadistics/page.jsx
+++ src/app/(singlegrid)/stadistics/page.jsx
@@ -20,7 +20,11 @@ export default function Stadistics() {
       <Header></Header>
       <div className="w-full flex justify-center items-center">
         <div
-          className="w-1/4 flex justify-center items-center bg-yellow-100 border border-yellow-500 rounded-lg hover:bg-yellow-200 hover:cursor-pointer m-2"
+          className={`w-1/4 flex justify-center items-center rounded-lg m-2 ${
+            filter === 0
+              ? "bg-yellow-100 border border-yellow-500"
+              : "bg-white hover:gray-100 hover:cursor-pointer shadow-lg"
+          }`}
           id="0"
           onClick={updateFilter}
         >
@@ -29,7 +33,11 @@ export default function Stadistics() {
           </button>
         </div>
         <div
-          className="w-1/4 flex justify-center items-center bg-cyan-100 border border-cyan-500 rounded-lg hover:bg-cyan-200 hover:cursor-pointer m-2"
+          className={`w-1/4 flex justify-center items-center rounded-lg m-2 ${
+            filter === 1
+              ? "bg-cyan-100 border border-cyan-500"
+              : "bg-white hover:gray-100 hover:cursor-pointer shadow-lg"
+          }`}
           id="1"
           onClick={updateFilter}
         >
@@ -38,7 +46,11 @@ export default function Stadistics() {
           </button>
         </div>
         <div
-          className="w-1/4 flex justify-center items-center bg-emerald-100 border border-emerald-500 rounded-lg hover:bg-emerald-200 hover:cursor-pointer m-2"
+          className={`w-1/4 flex justify-center items-center rounded-lg m-2 ${
+            filter === 2
+              ? "bg-emerald-100 border border-emerald-500"
+              : "bg-white hover:gray-100 hover:cursor-pointer shadow-lg"
+          }`}
           id="2"
           onClick={updateFilter}
         >
@@ -47,7 +59,16 @@ export default function Stadistics() {
           </button>
         </div>
       </div>
-      <BlocksDropdown></BlocksDropdown>
+      {/* <BlocksDropdown></BlocksDropdown> */}
+      <div className="w-full flex justify-end items-center">
+        <button
+          className="w-1/6 mr-5 p-1 flex justify-center items-center rounded-lg bg-sky-200 hover:bg-sky-300 hover:cursor-pointer"
+          type="text"
+          aria-haspopup="true"
+        >
+          <p>Select blocks</p>
+        </button>
+      </div>
       <div className="w-full h-screen flex justify-center items-center mt-2 mb-2">
         <div className="w-full h-full flex justify-center items-center m-2 rounded-lg shadow-lg bg-white"></div>
       </div>
