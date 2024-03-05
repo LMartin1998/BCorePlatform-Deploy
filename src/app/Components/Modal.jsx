ad3a233cec6b722d58a0676a5426c32a75861082
Jerónimo Molina <jerespartan117@hotmail.com>
2024-03-05 16:25:37 -0600

    handle click

--- src/app/Components/Modal.jsx
+++ src/app/Components/Modal.jsx
@@ -2,10 +2,18 @@ import ReactDOM from "react-dom";
 import styles from "@/app/styles/SlideTop.module.css";
 
 export default function Modal({ onClose, children }) {
+  const handleClose = (e) => {
+    e.preventDefault();
+    onClose();
+  };
+
   const modalContent = (
     <dialog className={`fixed z-50 ${styles.slide_top}`} open>
       <div className="w-full h-full rounded-lg shadow-lg bg-white">
-        <div className="flex w-full justify-center items-center border-b border-gray-300">
+        <div
+          className="flex w-full justify-center items-center border-b border-gray-300"
+          onClick={handleClose}
+        >
           {children}
         </div>
       </div>
