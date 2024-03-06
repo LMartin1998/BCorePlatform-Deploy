2a60dae68b70c012cb197dac4729226a2fa4976d
Jerónimo Molina <jerespartan117@hotmail.com>
2024-03-06 11:13:57 -0600

    Working on show files for children

--- src/app/(singlegrid)/docs/folder/[id]/page.jsx
+++ src/app/(singlegrid)/docs/folder/[id]/page.jsx
@@ -18,18 +18,27 @@ export default function Folder() {
     searchChildren,
     childrenType,
     updateChildrenType,
+    image,
+    showImage,
+    setShowImage,
   } = useContext(FilesContext);
 
   return (
-    <main className="bg-gray-100 min-h-screen">
-      <Header></Header>
-      <TopCards></TopCards>
-      <SearchFiles searchPage={searchChildren}></SearchFiles>
-      <div className="flex w-full m-1 p-1">
+    <main className="bg-gray-100 h-screen max-h-screen">
+      <div className="h-[3%]">
+        <Header></Header>
+      </div>
+      <div className="h-[20%]">
+        <TopCards></TopCards>
+      </div>
+      <div className="h-[6%] pt-1">
+        <SearchFiles searchPage={searchChildren}></SearchFiles>
+      </div>
+      <div className="flex w-full h-[5%]">
         <BreadcrumFiles></BreadcrumFiles>
         <DesignFiles></DesignFiles>
       </div>
-      <div className="flex w-1/3 m-2 justify-center items-center">
+      <div className="flex w-1/3 m-1 justify-center items-center h-[5%]">
         <AddFiles></AddFiles>
         <FilterFiles
           type={childrenType}
@@ -40,6 +49,20 @@ export default function Folder() {
         filesList={filterChildren}
         updateFiles={updateChildrenFiles}
       ></ShowFiles>
+      {showImage && (
+        <Modal
+          onClose={() => {
+            setShowImage(false);
+          }}
+          children={
+            <img
+              className="flex w-full h-full"
+              loading="lazy"
+              src={image}
+            ></img>
+          }
+        ></Modal>
+      )}
     </main>
   );
 }
