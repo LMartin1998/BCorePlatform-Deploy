f0204cfb07ebbc4b1aa3ab0b367e6ef7b10766a0
Jerónimo Molina <jerespartan117@hotmail.com>
2024-03-07 16:34:32 -0600

    Working on responsive page

--- src/app/(singlegrid)/docs/page.jsx
+++ src/app/(singlegrid)/docs/page.jsx
@@ -29,7 +29,7 @@ export default function Docs() {
   }, []);
 
   return (
-    <main className="bg-gray-100 h-screen min-h-screen">
+    <main className="bg-gray-100 h-full md:h-screen min-h-screen">
       <Header></Header>
       <TopCards></TopCards>
       <div className="flex items-center w-full">
@@ -43,7 +43,7 @@ export default function Docs() {
         <AddFiles></AddFiles>
         <FilterFiles type={mainType} updateType={updateMainType}></FilterFiles>
       </div>
-      <div className="flex w-full h-[50%]">
+      <div className="flex w-full h-96 md:h-[55%]">
         <ShowFiles
           filesList={filterMain}
           updateFiles={updateMainFiles}
