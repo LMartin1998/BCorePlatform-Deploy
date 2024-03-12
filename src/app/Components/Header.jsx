93e6de4031edf6a0dc5ce7f7480797572ed75654
Jerónimo Molina <jerespartan117@hotmail.com>
2024-03-12 12:11:21 -0600

    Adding dark theme

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -1,13 +1,60 @@
-import React from "react";
+import { useState } from "react";
+import { IoSettingsOutline } from "react-icons/io5";
+import SettingsModal from "./SettingsModal";
 
 const Header = () => {
-    return(
-        <div className='flex justify-between px-4 pt-4'>
-            <h2>B-Core platform</h2>
-            <h2>Welcome back, user</h2>
-        </div>
-    );
-}
+  const [open, setOpen] = useState(false);
+  const [theme, setTheme] = useState("white");
 
+  return (
+    <div className="flex items-center justify-between px-4 pt-4">
+      <div className="flex items-center">
+        <h2>B-Core platform</h2>
+      </div>
+      <div className="flex items-center">
+        <h2>Welcome back, user</h2>
+        <IoSettingsOutline
+          className="ml-2 hover:cursor-pointer"
+          size={20}
+          onClick={(e) => {
+            e.stopPropagation();
+            setOpen(true);
+          }}
+        ></IoSettingsOutline>
+      </div>
+      {open && (
+        <SettingsModal onClose={() => {}}>
+          <div className="flex flex-col w-full h-5/6">
+            <div className="flex items-center justify-between mt-4 m-2">
+              <p>Toggle theme</p>
+              <div
+                className={`cursor-pointer relative ${theme === "white" ? "bg-blue-500" : "bg-gray-400"} rounded-full w-8 h-4 transition`}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setTheme((prevTheme) =>
+                    prevTheme === "white" ? "dark" : "white",
+                  );
+                }}
+              >
+                <div
+                  className={`absolute ${theme === "white" ? "translate-x-4" : "translate-x-0"} left-0 bg-white w-4 h-4 rounded-full shadow-md transition transform duration-300 ease-in-out`}
+                />
+              </div>
+            </div>
+            <button
+              className="flex w-1/3 py-1 items-center justify-center self-center rounded-lg bg-blue-500 text-white"
+              onClick={(e) => {
+                e.stopPropagation();
+                setOpen(false);
+              }}
+            >
+              Close
+            </button>
+          </div>
+        </SettingsModal>
+      )}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
