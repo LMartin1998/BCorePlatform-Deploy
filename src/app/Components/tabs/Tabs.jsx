71d169861237ff582ec5382e87bfc815b24f9cd9
Jerónimo Molina <jerespartan117@hotmail.com>
2024-03-13 15:03:17 -0600

    Minor updated

--- src/app/Components/tabs/Tabs.jsx
+++ src/app/Components/tabs/Tabs.jsx
@@ -4,7 +4,7 @@ const Tab = ({ label, isActive, onClick }) => {
       className={`${
         isActive
           ? "border-b-2 border-blue-500 focus:outline-none text-blue-500 font-semibold dark:text-indigo-400 dark:border-indigo-400"
-          : "border-b border-transparent focus:outline-none dark:text-white"
+          : "border-b border-transparent focus:outline-none dark:text-[#A0AEC0]"
       } py-2 px-4 `}
       onClick={onClick}
     >
