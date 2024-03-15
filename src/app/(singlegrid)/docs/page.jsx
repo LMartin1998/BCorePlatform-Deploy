38ca06887faefc7e6d5a99addc804f7eed98974d
Jerónimo Molina <jerespartan117@hotmail.com>
2024-03-15 10:30:35 -0600

    main files docs complete

--- src/app/(singlegrid)/docs/page.jsx
+++ src/app/(singlegrid)/docs/page.jsx
@@ -40,41 +40,48 @@ export default function Docs() {
   }, [theme]);
 
   return (
-    <main className="bg-gray-100 h-screen min-h-screen dark:bg-[#1A202C]">
-      <Header></Header>
-      <TopCards></TopCards>
-      <div className="flex items-center w-full">
-        <SearchFiles searchPage={searchMain}></SearchFiles>
-      </div>
-      <div className="flex w-full justify-between items-center">
-        <BreadcrumFiles></BreadcrumFiles>
-        <DesignFiles></DesignFiles>
-      </div>
-      <div className="flex w-full lg:w-1/3 m-1 justify-center items-center">
-        <AddFiles></AddFiles>
-        <FilterFiles type={mainType} updateType={updateMainType}></FilterFiles>
-      </div>
-      <div className="flex w-full h-96 sm:h-[42%] 2xl:h-[47%]">
-        <ShowFiles
-          filesList={filterMain}
-          updateFiles={updateMainFiles}
-        ></ShowFiles>
+    <main className="bg-gray-100 h-screen min-h-screen dark:bg-[#1A202C] overflow-hidden">
+      <div className="w-full h-full overflow-auto">
+        <Header></Header>
+        <TopCards></TopCards>
+        <div className="flex items-center w-full">
+          <SearchFiles searchPage={searchMain}></SearchFiles>
+        </div>
+        <div className="flex w-full justify-between items-center">
+          <BreadcrumFiles></BreadcrumFiles>
+          <DesignFiles></DesignFiles>
+        </div>
+        <div className="flex w-full lg:w-1/3 m-1 justify-center items-center">
+          <AddFiles></AddFiles>
+          <FilterFiles
+            type={mainType}
+            updateType={updateMainType}
+          ></FilterFiles>
+        </div>
+        <div className="flex w-full h-[70%] bg-gray-100 dark:bg-[#1A202C]">
+          <ShowFiles
+            filesList={filterMain}
+            updateFiles={updateMainFiles}
+          ></ShowFiles>
+        </div>
       </div>
       {showImage && (
-        <Modal
-          onClose={() => {
-            setShowImage(false);
-          }}
-        >
-          <div className="flex w-6/12 h-5/6 rounded-lg shadow-lg bg-white z-50">
-            <img
-              className="flex w-full h-full object-cover"
-              loading="lazy"
-              alt="image"
-              src={image}
-            ></img>
-          </div>
-        </Modal>
+        <div className="absolute left-0 top-0 flex w-full h-full">
+          <Modal
+            onClose={() => {
+              setShowImage(false);
+            }}
+          >
+            <div className="flex w-6/12 h-5/6 rounded-lg shadow-lg bg-white z-50">
+              <img
+                className="flex w-full h-full object-cover"
+                loading="lazy"
+                alt="image"
+                src={image}
+              ></img>
+            </div>
+          </Modal>
+        </div>
       )}
     </main>
   );
