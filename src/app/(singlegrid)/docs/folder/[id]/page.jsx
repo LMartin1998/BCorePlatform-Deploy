d0ae50649dff001803c4cd062bad6472f6b3aa26
Jerónimo Molina <jerespartan117@hotmail.com>
2024-03-15 10:47:33 -0600

    Drive completed

--- src/app/(singlegrid)/docs/folder/[id]/page.jsx
+++ src/app/(singlegrid)/docs/folder/[id]/page.jsx
@@ -25,43 +25,47 @@ export default function Folder() {
   } = useContext(FilesContext);
 
   return (
-    <main className="bg-gray-100 h-screen min-h-screen dark:bg-[#1A202C]">
-      <Header></Header>
-      <TopCards></TopCards>
-      <div className="flex items-center w-full">
-        <SearchFiles searchPage={searchChildren}></SearchFiles>
-      </div>
-      <div className="flex w-full justify-between items-center">
-        <BreadcrumFiles></BreadcrumFiles>
-        <DesignFiles></DesignFiles>
-      </div>
-      <div className="flex w-full lg:w-1/3 m-1 justify-center items-center">
-        <AddFiles></AddFiles>
-        <FilterFiles
-          type={childrenType}
-          updateType={updateChildrenType}
-        ></FilterFiles>
-      </div>
-      <div className="flex w-full h-96 sm:h-[42%] 2xl:h-[47%]">
-        <ShowFiles
-          filesList={filterChildren}
-          updateFiles={updateChildrenFiles}
-        ></ShowFiles>
+    <main className="bg-gray-100 h-screen min-h-screen dark:bg-[#1A202C] overflow-hidden">
+      <div className="w-full h-full overflow-auto">
+        <Header></Header>
+        <TopCards></TopCards>
+        <div className="flex items-center w-full">
+          <SearchFiles searchPage={searchChildren}></SearchFiles>
+        </div>
+        <div className="flex w-full justify-between items-center">
+          <BreadcrumFiles></BreadcrumFiles>
+          <DesignFiles></DesignFiles>
+        </div>
+        <div className="flex w-full lg:w-1/3 m-1 justify-center items-center">
+          <AddFiles></AddFiles>
+          <FilterFiles
+            type={childrenType}
+            updateType={updateChildrenType}
+          ></FilterFiles>
+        </div>
+        <div className="flex w-full h-[70%] bg-gray-100 dark:bg-[#1A202C]">
+          <ShowFiles
+            filesList={filterChildren}
+            updateFiles={updateChildrenFiles}
+          ></ShowFiles>
+        </div>
       </div>
       {showImage && (
-        <Modal
-          onClose={() => {
-            setShowImage(false);
-          }}
-        >
-          <div className="flex w-6/12 h-5/6 rounded-lg shadow-lg bg-white z-50">
-            <img
-              className="flex w-full h-full object-cover"
-              loading="lazy"
-              src={image}
-            ></img>
-          </div>
-        </Modal>
+        <div className="absolute left-0 top-0 flex w-full h-full">
+          <Modal
+            onClose={() => {
+              setShowImage(false);
+            }}
+          >
+            <div className="flex w-6/12 h-5/6 rounded-lg shadow-lg bg-white z-50">
+              <img
+                className="flex w-full h-full object-cover"
+                loading="lazy"
+                src={image}
+              ></img>
+            </div>
+          </Modal>
+        </div>
       )}
     </main>
   );
